f569a8fc5106b8c3f8e4dd50073ca074e7356e52
akinsola akinwale <faskinss@gmail.com>
2019-12-05 17:21:23 +0100

    REF: DockerHub ref

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,3 @@
-docker login --username $DOCKER_USER --password $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-TAG="latest"
-else
-TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
-docker push $DOCKER_REPO
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push "$DOCKER_USER"/"$DOCKER_REPO"
